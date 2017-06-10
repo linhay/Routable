e3ee0794d09c872aac82e4960cc32c6cf89459df
BigL <158179948@qq.com>
2017-06-10 16:26:03 +0800

    初始化

--- Routable.podspec
+++ Routable.podspec
@@ -30,7 +30,7 @@ TODO: Add long description of the pod here.
 
   s.ios.deployment_target = '8.0'
 
-  s.source_files = 'Routable/Classes/**/*'
+  s.source_files = 'Source/**/*'
   
   # s.resource_bundles = {
   #   'Routable' => ['Routable/Assets/*.png']
---
+++ Source/Routable.swift
@@ -0,0 +1,100 @@
+//
+//  Router.swift
+//  Router
+//
+//  Created by BigL on 2017/3/21.
+//  Copyright © 2017年 fun.bigl.com. All rights reserved.
+//
+
+import UIKit
+
+
+public class Routable {
+
+  fileprivate let namespace = Bundle.main.infoDictionary?["CFBundleExecutable"] as! String
+  public static let shared = Routable()
+  fileprivate lazy var cachedTarget = [String: Any]()
+
+}
+
+public extension Routable {
+
+  public func releaseCached(targetName: String) {
+    let targetClassString =  "Router_" + targetName
+    cachedTarget.removeValue(forKey: targetClassString)
+  }
+
+  public func performTarget(name: String,
+                            actionName: String,
+                            params: [String: Any] = [:],
+                            isCacheTarget: Bool) -> AnyObject? {
+
+    let targetClassString = namespace + "." + "Router_" + name
+    let actionString = "router_\(actionName)WithParams:"
+    var targetClass: NSObject.Type?
+    var target = cachedTarget[targetClassString]
+
+    if target == nil {
+      targetClass = NSClassFromString(targetClassString) as? NSObject.Type
+      target = targetClass?.init()
+    }
+
+    let action = NSSelectorFromString(actionString)
+
+    if target == nil { return nil }
+
+    if isCacheTarget { cachedTarget[targetClassString] = target }
+
+    if true {
+      guard let target = target as? NSObject else { return nil }
+      switch target.responds(to: action) {
+      case true:
+        return target.perform(action, with: params).takeUnretainedValue()
+      case false:
+        let actionString = "router_\(actionName)Params:"
+        var action = NSSelectorFromString(actionString)
+        switch target.responds(to: action) {
+        case true:
+          return target.perform(action, with: params).takeUnretainedValue()
+        case false:
+          action = NSSelectorFromString("notFound:")
+          switch target.responds(to: action) {
+          case true:
+            return target.perform(action, with: params).takeUnretainedValue()
+          case false:
+            cachedTarget.removeValue(forKey: targetClassString)
+            return nil
+          }
+        }
+      }
+    }
+  }
+
+  public func performAction(url: URL, completion: (([String: Any]) -> ())?) -> AnyObject? {
+    var params = [String: Any]()
+    var urlstr = ""
+
+    if let query = url.query { urlstr = query.removingPercentEncoding ?? "" }
+    urlstr.components(separatedBy: "&").forEach { (item) in
+      let list = item.components(separatedBy: "=")
+      if list.count < 2 { return }
+      params[list.first!] = list.last!
+    }
+
+    let actionName = url.path.replacingOccurrences(of: "/", with: "")
+
+    let result = performTarget(name: url.host!,
+                               actionName: actionName,
+                               params: params,
+                               isCacheTarget: false)
+
+    completion?(["result": result ?? false])
+    return result
+  }
+  
+}
+
+
+
+
+
