25f8af59b61ba475a11d27fc3e4d1fe050a53e5b
linhey <158179948@qq.com>
2018-01-25 11:50:17 +0800

    添加提交脚本

--- trunk.sh
+++ trunk.sh
@@ -2,25 +2,25 @@
 #value=$(<uppod.sh)
 #echo "$value"
 
-value=./*.podspec
-file=${value##*/}
+# value=./*.podspec
+file="SPRoutable.podspec" #${value##*/}
 
-echo "podspec:"  ${file[*]}
+echo "podspec:"  ${file}
 #获取podspec 中版本号
-version=`grep -m1 "s.version" $file|awk '{print $NF}'|sed "s/\'//g"`
-echo "version:"  $version
+version=`grep -m1 "s.version" ${file}|awk '{print $NF}'|sed "s/\'//g"`
+echo "version:"  ${version}
 #git提交
 git add .
 read -t 600 -p "修改内容[默认为${version}]:" diff
-if [ -z $diff ];then  
-    diff=$version
+if [ -z ${diff} ];then  
+    diff=${version}
 fi 
-git commit -m $diff
+git commit -m ${diff}
 git push
 read -t 600 -p "版本内容[默认为${version}]:" diff
-if [ -z $diff ];then  
-    diff=$version
+if [ -z ${diff} ];then  
+    diff=${version}
 fi 
-git tag -a $version -m $diff
-git push origin $version
-pod trunk push $file
+git tag -a ${version} -m ${diff}
+git push origin ${version}
+pod trunk push ${file}
